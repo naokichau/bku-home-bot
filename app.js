3e8e38684a1584ab6563998cd9779485a3e7edb6
Naoki Châu <Naoki Chu>
2017-03-17 15:40:56 +0700

    new

--- app.js
+++ app.js
@@ -363,7 +363,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, "Hi, " + event.sender.name +" connect your sensePods on sens.io to start monitor your house.");
 }
 
 /*
