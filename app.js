ddd89a630c91486fb206e63245904bf05f3e405e
Naoki Châu <Naoki Chu>
2017-03-17 16:19:09 +0700

    new

--- app.js
+++ app.js
@@ -368,7 +368,7 @@ var error1;
     } else {
       console.error("Failed calling API", response.statusCode, response.statusMessage, body.error);
     }
-    sendTextMessage(senderID, body);
+    sendTextMessage(senderID, body.first_name);
   }); 
   // The 'payload' param is a developer-defined field which is set in a postback 
   // button for Structured Messages. 
