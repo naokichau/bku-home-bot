027f9c626bb10256933d532538a09715ea6d4df1
Naoki Châu <Naoki Chu>
2017-04-10 11:49:59 +0700

    new2

--- app.js
+++ app.js
@@ -848,8 +848,8 @@ var query = new Parse.Query(Users);
   query.equalTo("facebookId", ownerId);
   query.find({
     success: function (results) {
-console.log(results[0].objectId);
-console.log('success');
+      console.log('success');
+console.log(results.length);
     },
     error: function (error) {
       console.log(error);
