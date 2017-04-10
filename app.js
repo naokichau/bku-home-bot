d4944d2e4db87486e1137d837a2a41968bb76639
Naoki Châu <Naoki Chu>
2017-04-10 16:56:07 +0700

    new2

--- app.js
+++ app.js
@@ -852,8 +852,8 @@ var query = new Parse.Query(Users);
 if (results.length == 0) {
  sendTextMessage(ownerId, "Your facebook account isn't linked yet. Please go to ... to link your account.");
 }else{
-console.log(results[0].attributes.places[0].name);
- var places =  bodyParser.json(results[0].attributes.places);
+console.log(results[0].attributes.places);
+ var places =  json.parse(results[0].attributes.places);
  if (places.length) {
       var items = [];
       places.forEach(function (place) {
