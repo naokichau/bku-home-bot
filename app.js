40142c67fe3bcaf03cfc5ad07a58d4bcde44f07f
Naoki Châu <Naoki Chu>
2017-04-10 11:56:22 +0700

    new2

--- app.js
+++ app.js
@@ -849,7 +849,7 @@ var query = new Parse.Query(Users);
   query.find({
     success: function (results) {
       console.log(results.length);
-console.log(results[0].attributes.username);
+console.log(results[0].objectid);
     },
     error: function (error) {
       console.log(error);
